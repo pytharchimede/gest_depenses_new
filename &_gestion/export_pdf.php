f46387a9e3c47fcd9ca90a1f8ba0968128300f44
pytharchimede <amani_ulrich@outlook.fr>
2025-04-09 11:57:53 +0000

    Affichage de l'affectation

--- &_gestion/export_pdf.php
+++ &_gestion/export_pdf.php
@@ -1,6 +1,12 @@
 <?php
 
+require_once 'model/Database.php';
+require_once 'model/Affectation.php';
 require_once 'tcpdf/tcpdf.php';
+
+$databaseObj = new Database();
+$pdo = $databaseObj->getConnection();
+$affectationObj = new Affectation($pdo);
 
 $data = json_decode(file_get_contents('php://input'), true);
 $fiches = $data['fiches'] ?? [];
@@ -25,7 +31,7 @@ foreach ($fiches as $fiche) {
     $html .= "<tr>
         <td>{$fiche['beficiaire_fiche']}</td>
         <td>{$fiche['montant_fiche']}</td>
-        <td>{$fiche['affectation']}</td>
+        <td>{$affectationObj->getAffectationById($fiche['affectation_id'])['lib_affectation']}</td>
         <td>{$fiche['num_piece']}</td>
         <td>{$fiche['designation_fiche']}</td>
         <td>{$fiche['date_creat_fiche']}</td>
